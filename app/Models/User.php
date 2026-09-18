cb439624f729bc057ca013f31eb09c32287976a1
me-dav <000david12345go@gmail.com>
2026-09-18 09:46:18 +0700

    add ticket model

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
